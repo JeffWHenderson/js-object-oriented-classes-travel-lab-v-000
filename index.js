e30ae9680f903babae11d4fd85c7009d8e1438ea
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-22 21:41:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ class Route {
 
     blocksTravelled() {
       this.beginningLocation['horizontal']
-      return this.beginningLocation['vertical'] - this.ending['vertical']
+      return this.beginningLocation//['vertical'] - this.ending['vertical']
     }
 }
