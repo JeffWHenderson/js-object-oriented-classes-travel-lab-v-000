77de47612f75ca33721b22d86082e4f5a445d4b7
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-22 21:38:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 class Route {
-  let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
 
-
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+  
+    blocksTravelled() {
+      
+    }
 }
 
 class Driver {
